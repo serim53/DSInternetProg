47aafe9b935e5c72926fe469b37f195bd7e34989
serim53 <tpfla9953@naver.com>
2021-09-29 18:18:19 +0900

    CBV로 블로그 페이지 만들기

--- blog/views.py
+++ blog/views.py
@@ -1,21 +1,34 @@
 from django.shortcuts import render
+from django.views.generic import ListView, DetailView
+
 from .models import Post
 
 # Create your views here.
-def index(request) :
-    # pk를 기준으로 정렬을 하고 거꾸로 해주기 위해 앞에 -를 붙여줌
-    # 이렇게 해야 글이 최신순으로 보여짐
-    posts = Post.objects.all().order_by('-pk')
-    return render(request, 'blog/index.html',
-                  {
-                      'posts' : posts
-                  }
-                  )
 
-def single_post_page(request, pk) :
-    post = Post.objects.get(pk=pk)
-    return render(request, 'blog/single_post_page.html',
-                  {
-                      'post': post
-                  }
-                  )
+class PostList(ListView) :
+    model = Post
+    ordering = '-pk'
+#    template_name = 'blog/post_list.html'
+# post_list.html
+
+class PostDetail(DetailView) :
+    model = Post
+# post_detail.html
+
+# def index(request) :
+#     # pk를 기준으로 정렬을 하고 거꾸로 해주기 위해 앞에 -를 붙여줌
+#     # 이렇게 해야 글이 최신순으로 보여짐
+#     posts = Post.objects.all().order_by('-pk')
+#     return render(request, 'blog/post_list.html',
+#                   {
+#                       'posts' : posts
+#                   }
+#                   )
+
+# def single_post_page(request, pk) :
+#     post = Post.objects.get(pk=pk)
+#     return render(request, 'blog/post_detail.html',
+#                   {
+#                       'post': post
+#                   }
+#                   )
