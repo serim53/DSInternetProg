54e77b730ecec2c8affe65c36959e648b7472949
serim53 <tpfla9953@naver.com>
2021-11-10 12:12:57 +0900

    포스트 수정 기능 추가하기

--- blog/urls.py
+++ blog/urls.py
@@ -4,6 +4,8 @@ from . import views
 urlpatterns = [ # 서버 ip/blog/
     #path('<int:pk>/', views.single_post_page),
     #path('', views.index),
+
+    path('update_post/<int:pk>/', views.PostUpdate.as_view()),
     path('create_post/',views.PostCreate.as_view()),
     path('tag/<str:slug>',views.tag_page),
     path('category/<str:slug>',views.category_page), # 서버 ip/blog/category/slug
--- blog/views.py
+++ blog/views.py
@@ -1,5 +1,6 @@
+from django.core.exceptions import PermissionDenied
 from django.shortcuts import render, redirect
-from django.views.generic import ListView, DetailView,CreateView
+from django.views.generic import ListView, DetailView, CreateView, UpdateView
 from .models import Post, Category, Tag
 from django.contrib.auth.mixins import LoginRequiredMixin, UserPassesTestMixin
 
@@ -20,6 +21,18 @@ class PostCreate(LoginRequiredMixin, UserPassesTestMixin, CreateView):
         else :
             return redirect('/blog/')
 
+class PostUpdate(LoginRequiredMixin, UpdateView):   # 모델명_form
+    model = Post
+    fields = ['title', 'hook_text', 'content', 'head_image', 'file_upload', 'category', 'tags']
+
+    template_name = 'blog/post_update_form.html'
+
+
+    def dispatch(self, request, *args, **kwargs):
+        if request.user.is_authenticated and request.user == self.get_object().author :
+            return super(PostUpdate, self).dispatch(request, *args, **kwargs)
+        else :
+            raise PermissionDenied
 
 class PostList(ListView) :
     model = Post
