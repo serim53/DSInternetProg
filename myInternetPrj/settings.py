2c35458ef747681752748ee958fc18df3daca739
serim53 <tpfla9953@naver.com>
2021-11-29 13:37:17 +0900

    도커에 장고만 담기

--- myInternetPrj/settings.py
+++ myInternetPrj/settings.py
@@ -20,12 +20,15 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-#@8hmm-kaa47_=!0*@^m&07o0dj2-@2oza)9=nyk%z8*963ew8'
+SECRET_KEY = os.environ.get('SECRET_KEY', 'django-insecure-#@8hmm-kaa47_=!0*@^m&07o0dj2-@2oza)9=nyk%z8*963ew8')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = int(os.environ.get('DEBUG', 1))
 
-ALLOWED_HOSTS = []
+if os.environ.get('DJANGO_ALLOWED_HOSTS'):
+    ALLOWED_HOSTS = os.environ.get('DJANGO_ALLOWED_HOSTS').split(' ')
+else :
+    ALLOWED_HOSTS = []
 
 
 # Application definition
